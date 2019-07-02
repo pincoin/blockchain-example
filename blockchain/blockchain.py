f964f6b368c6c329a951ec5532e9b909032a61df
pincoin <pincoins@gmail.com>
2019-07-03 00:32:35 +0900

    - comment added

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,7 +1,21 @@
 class Block:
-    def __init__(self, index, hash, previous_block_hash, timestamp, data):
+    def __init__(self, index, hash, previous_block_hash, timestamp, data, difficulty, nonce):
+        """Constructor for block
+
+        Parameters
+        ----------
+        index : block counter
+        hash : current block hash
+        previous_block_hash : previous block hash to mingle with current block hash
+        timestamp : timestamp when block is created
+        data : block data such as transactions
+        difficulty :
+        nonce :
+        """
         self.index = index
         self.hash = hash
         self.previous_block_hash = previous_block_hash
         self.timestamp = timestamp
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
