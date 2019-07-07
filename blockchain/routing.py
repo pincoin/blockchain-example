0062ebec6ea4e3b65cfeac2c01aaaf14c801d908
pincoin <pincoins@gmail.com>
2019-07-07 16:22:13 +0900

    - path url

--- blockchain/routing.py
+++ blockchain/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/blockchain/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    path('ws/blockchain/<str:room_name>/', consumers.ChatConsumer),
 ]
